9f37284c35328d111c26391760e5c9f2b7d23b0c
sgwicks <samgwicks@gmail.com>
2020-07-29 09:14:27 +0100

    Learning lazy seqs, into, conj, complement

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -187,3 +187,33 @@
     ;       (into final-body-parts (set [part (matching-part part)])))
     ;     []
     ;     body-parts))
+
+    (def vampire-database
+      {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+       1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+       2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+       3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+    
+    (defn vampire-related-details
+      [social-security-number]
+      (Thread/sleep 1000)
+      (get vampire-database social-security-number))
+    
+    (defn vampire?
+      [record]
+      (and (:makes-blood-puns? record)
+           (not (:has-pulse? record))
+           record))
+    
+    (defn identify-vampire
+      [social-security-numbers]
+      (first (filter vampire?
+                     (map vampire-related-details social-security-numbers))))
+    
+    (def not-vampire? (complement vampire?))
+
+    (defn identify-humans
+      [social-security-numbers]
+      (filter (complement vampire?)
+        (map vampire-related-details social-security-numbers)
+      ))
